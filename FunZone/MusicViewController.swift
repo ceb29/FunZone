7b86e3d8620647b5a61c900495ebb4ee1b2812ec
Curtis Becker <ceb2941@gmail.com>
2022-06-01 18:10:11 -0400

    added music player methods

--- FunZone/MusicViewController.swift
+++ FunZone/MusicViewController.swift
@@ -6,9 +6,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MusicViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     @IBOutlet weak var musicTableView: UITableView!
+    @IBOutlet weak var songTitleLabel: UILabel!
+    var musicPlayer : AVAudioPlayer?
+    var musicFile : String?
+    var currentSong = ""
     var dataText = ["song1", "song2", "song3", "song4", "song5", "song6", "song7", "song8", "song9", "song10", "song11", "song12", "song13", "song14", "song15"]
     var searchResultDataText : [String] = []
     
@@ -30,6 +35,33 @@ class MusicViewController: UIViewController, UITableViewDataSource, UITableViewD
             searchResultDataText = dataText.filter {(str : String) -> Bool in return str.lowercased().contains(searchText.lowercased())}
         }
         musicTableView.reloadData()
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        currentSong = searchResultDataText[indexPath.item]
+        songTitleLabel.text = currentSong
+    }
+    
+    func setupMusicPlayer(){
+        let filePath = Bundle.main.path(forResource: "sample", ofType: "mp3")
+        let url = URL(fileURLWithPath: filePath!)
+        do{
+            musicPlayer = try AVAudioPlayer(contentsOf: url)
+        }
+        catch{
+            print("file not found")
+        }
+    }
+    
+    @IBAction func playMusic(_ sender: Any) {
+        musicPlayer?.stop()
+        print("music stopped playing")
+    }
+    
+    @IBAction func stopMusic(_ sender: Any) {
+        musicFile = "sol"
+        musicPlayer?.play()
+        print("music is playing")
     }
     
     override func viewDidLoad() {
