f218043b3f0ea4186ddb9a0ed63cbbbc862d9a1b
Curtis Becker <ceb2941@gmail.com>
2022-06-04 19:19:27 -0400

    changed toggle state to start with on

--- FunZone/LoginViewController.swift
+++ FunZone/LoginViewController.swift
@@ -11,7 +11,7 @@ class LoginViewController: UIViewController {
     @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var resultLabel: UILabel!
-    var rememberIsOn = false
+    var rememberIsOn = true
     let userDefault = UserDefaults.standard
     
     override func viewDidLoad() {
