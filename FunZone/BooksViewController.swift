eb7e8881efafabbcb6c7bd9bbc13ac3892642898
Curtis Becker <ceb2941@gmail.com>
2022-06-04 19:19:08 -0400

    added images for books in collection view

--- FunZone/BooksViewController.swift
+++ FunZone/BooksViewController.swift
@@ -10,16 +10,11 @@ import UIKit
 class BooksViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource, UISearchBarDelegate{
     @IBOutlet weak var booksCollectionView: UICollectionView!
     var dataText = ["Basic Operators", "Closures", "Collection Types", "Control Flow", "Enumerations", "Error Handling", "Extensions", "Functions", "Inheritance", "Initialization", "Methods", "Nested Types", "Optional Chaining", "Properties", "Structures and Classes"]
-    //var dataImg = ["img1", "img1", "img1", "img1", "img1", "img1", "img1", "img1", "img1", "img1", "img1", "img1", "img1", "img1", "img1"]
     var searchResultDataText : [String] = []
-    var searchResultDataImg : [String] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
         searchResultDataText = dataText
-        //searchResultDataImg = dataImg
-        
-        // Do any additional setup after loading the view.
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
@@ -29,8 +24,8 @@ class BooksViewController: UIViewController, UICollectionViewDelegate, UICollect
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let myCell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! BooksCollectionViewCell
         myCell.bookLabel.text = searchResultDataText[indexPath.row]
-        //myCell.bookImg.image = UIImage(named: dataImg[indexPath.row])
-        myCell.backgroundColor = UIColor.gray
+        myCell.bookImg.image = UIImage(named: searchResultDataText[indexPath.row])
+        myCell.backgroundColor = UIColor.white
         myCell.layer.cornerRadius = 10
         myCell.layer.masksToBounds = true
         return myCell
