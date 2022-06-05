997c3ddc5aab293f04ef56bbde2355dc5c40e3a6
Curtis Becker <ceb2941@gmail.com>
2022-06-05 13:39:15 -0400

    added custom view for opening web page on current screen

--- FunZone/SearchViewController.swift
+++ FunZone/SearchViewController.swift
@@ -6,17 +6,36 @@
 //
 
 import UIKit
+import WebKit
 
 class SearchViewController: UIViewController{
-    
+    @IBOutlet weak var searchLabel: UILabel!
+    @IBOutlet weak var startButton: UIButton!
+    @IBOutlet weak var customView: UIView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        customView.isHidden = true
     }
     
     @IBAction func startSearching(_ sender: Any) {
+        startButton.isHidden = true
+        searchLabel.isHidden = true
+        setupWebView()
+    }
+    
+    func setupWebView(){
+        customView.isHidden = false
+        let newURL = URL(string : "https://www.google.com/")
+        let webKitView = WKWebView(frame: customView.bounds)
+        webKitView.load(URLRequest(url: newURL!))
+        customView.addSubview(webKitView)
+    }
+    
+    /*
+    func openInNewScreen(){
         let storyObject = UIStoryboard(name: "Main", bundle: nil)
         let searchStartedScreen = storyObject.instantiateViewController(withIdentifier: "SearchStart") as! SearchStartViewController
                 self.navigationController?.pushViewController(searchStartedScreen, animated: true)
     }
-    
+    */
 }
